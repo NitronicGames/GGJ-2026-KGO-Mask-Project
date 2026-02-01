6ec5e7f19e09225ca41f90c8e0ca6d59eca82c38
Nitronic Games <ethanweatherly06@gmail.com>
2026-02-01 04:59:52 -0500

    GameUI Rebuilt Panels and Scripts (Latest)

--- UI/game_ui.gd
+++ UI/game_ui.gd
@@ -1,4 +1,7 @@
 extends Control
+@onready var dialogue_panel: Control = $DialoguePanel
+@onready var choice_panel: Control = $ChoicePanel
+@onready var feedback_panel: Control = $FeedbackPanel
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,3 +16,31 @@ func _process(delta: float) -> void:
 
 func _on_continue_button_pressed() -> void:
 	print("Continue clicked")
+	dialogue_panel.visible = false
+	choice_panel.visible = true
+	feedback_panel.visible = false
+
+
+func _on_casual_button_pressed() -> void:
+	print("Mask: casual")
+	choice_panel.visible = false
+	feedback_panel.visible = true
+
+
+func _on_polite_button_pressed() -> void:
+	print("Mask: polite")
+	choice_panel.visible = false
+	feedback_panel.visible = true
+
+
+func _on_formal_button_pressed() -> void:
+	print("Mask: formal")
+	choice_panel.visible = false
+	feedback_panel.visible = true
+
+
+func _on_next_button_pressed() -> void:
+	print("Next clicked")
+	dialogue_panel.visible = true
+	choice_panel.visible = false
+	feedback_panel.visible = false
