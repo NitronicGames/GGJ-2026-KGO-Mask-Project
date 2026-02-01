2532e7156871f3a1960c62470da558bc397e0b2d
hatmix <67128187+hatmix@users.noreply.github.com>
2026-02-01 11:28:25 -0500

    Added npc scene link in npc game data

--- components/resource_scripts/npc.gd
+++ components/resource_scripts/npc.gd
@@ -1,7 +1,9 @@
 class_name NPC
 extends Resource
 
-## Should match dialogue speaker string
+## Used for dialogue speaker string
 @export var name: String
+## Scene file with visuals & animations
+@export_file("*.tscn") var scene_file: String
 ## List of reactions for this NPC, shown when the combination of expected and actual masks match
 @export var reactions: Array[DialogueReaction]
