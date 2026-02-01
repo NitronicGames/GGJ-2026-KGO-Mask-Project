a3a6f3e48d6efb183f5cedc2548b1e5ff76401ca
Nitronic Games <ethanweatherly06@gmail.com>
2026-02-01 01:14:57 -0500

    Main Menu UI
    Added main menu UI (Start, options, and exit buttons with pressed mechanic)

---
+++ main_menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://game_state/game_state.tscn")
+
+
+func _on_options_button_pressed() -> void:
+	print("Options Pressed")
+
+
+func _on_exit_button_pressed() -> void:
+	print("Exit Pressed")
+	get_tree().quit()
